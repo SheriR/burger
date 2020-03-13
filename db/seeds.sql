5cc52341d4c268847d8e4b737a324707986ce1ce
Sheri Rhoades <sheri_rhoades@yahoo.com>
2020-03-13 10:17:52 -0500

    connection made and added sql db and populated burger table with 3 burgers

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Cheese Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES('Bacon Cheese Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES('Chicken Bacon Ranch Burger');
